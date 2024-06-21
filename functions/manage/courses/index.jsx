61a2efa30591b9e23c8a273e3c2b01ead76c0f6e
Aakash N S <1560745+aakashns@users.noreply.github.com>
2024-06-21 20:32:58 +0530

    Enable sorting rows in manage courses table

--- functions/manage/courses/index.jsx
+++ functions/manage/courses/index.jsx
@@ -12,16 +12,52 @@ const { default: jsx } = require("lib/utils/jsx");
  * - [ ] Add pagiation beyond 1000 courses (at some point)
  */
 
-export const onRequestGet = safeguard(async function ({ request, env }) {
+export const onRequest = safeguard(async function ({ request, env }) {
+  if (!["GET", "POST"].includes(request.method)) {
+    return new Response("Method Not Allowed", { status: 405 });
+  }
   const { CACHE_KV: cacheKv, DB: database } = env;
   const siteSettings = await getSiteSettings({ cacheKv });
   const currentUser = await getCurrentUser({ request, database });
+  const baseProps = { siteSettings, currentUser };
   if (!currentUser || !currentUser?.is_admin)
-    return makeHtmlResponse(<NotFoundPage siteSettings={siteSettings} currentUser={currentUser} />);
+    return makeHtmlResponse(<NotFoundPage {...baseProps} />);
   const courses = await selectCoursesForAdmin({ database });
 
+  if (request.method == "GET") {
+    return makeHtmlResponse(<ManageCoursesPage {...baseProps} courses={courses} />);
+  }
+
+  // TODO - handle the case where form data does not contain sort order
+
+  const formData = await request.formData();
+  const courseIds = courses.map((c) => c.id);
+  const newSortedCourseIds = formData
+    .get("sorted_course_ids")
+    ?.trim()
+    .split(",")
+    .map((idStr) => parseInt(idStr))
+    .filter((id) => courseIds.includes(id));
+  const newSortOrders = courseIds
+    .map((id) => newSortedCourseIds.indexOf(id))
+    .map((x) => (x === -1 ? null : x));
+
+  const values = courseIds.map((id, i) => `(${id}, ${newSortOrders[i]})`).join(", ");
+
+  const query = `WITH idso(id, sort_order) AS (VALUES ${values})
+    UPDATE courses
+    SET sort_order = idso.sort_order
+    FROM idso
+    WHERE courses.id = idso.id;
+  `;
+
+  const result = await database.prepare(query).run();
+  const newCourses = await selectCoursesForAdmin({ database });
+
+  // TODO - Show success message that courses were sorted
+
   return makeHtmlResponse(
-    <ManageCoursesPage siteSettings={siteSettings} currentUser={currentUser} courses={courses} />
+    <ManageCoursesPage siteSettings={siteSettings} currentUser={currentUser} courses={newCourses} />
   );
 });
 
@@ -60,7 +96,7 @@ function ManageCoursesPage({ siteSettings, currentUser, courses }) {
             </thead>
             <tbody>
               {courses.map((course) => (
-                <tr>
+                <tr data-course-id={course.id}>
                   <td>{course.id}</td>
                   <td>{course.title}</td>
                   <td>{PrivacyDisplay[course.privacy]}</td>
@@ -76,15 +112,32 @@ function ManageCoursesPage({ siteSettings, currentUser, courses }) {
                           Manage
                         </a>
                       </li>
+                      <li>
+                        <span className="manage-courses-drag-handle">⋮⋮</span>
+                      </li>
                     </ul>
                   </td>
                 </tr>
               ))}
             </tbody>
           </table>
-          <a href={`/manage/courses/new`} className="ui-button">
-            + New Course
-          </a>
+          <div className="ui-button-row manage-courses-default-buttons">
+            <button className="ui-button-outline" onclick="ManageCourses.enableReorder(event);">
+              ⋮⋮ Reorder
+            </button>
+            <a href={`/manage/courses/new`} className="ui-button-outline">
+              + New Course
+            </a>
+          </div>
+          <div className="ui-button-row manage-courses-ordering-buttons">
+            <a href={`/manage/courses`} className="ui-button-outline">
+              ✕ Reset Order
+            </a>
+            <form action="/manage/courses" method="post">
+              <input type="hidden" name="sorted_course_ids" value="" />
+              <input type="submit" className="ui-button-outline" value="⋮⋮ Save Order" />
+            </form>
+          </div>
         </section>
       </main>
     </RootLayout>
@@ -94,5 +147,15 @@ function ManageCoursesPage({ siteSettings, currentUser, courses }) {
 async function selectCoursesForAdmin({ database }) {
   const query = `SELECT id, slug, title, privacy, sort_order FROM courses LIMIT 1000;`;
   const queryOutput = await database.prepare(query).all();
-  return queryOutput.results;
+  return sortCourses({ courses: queryOutput.results });
+}
+
+function sortCourses({ courses }) {
+  courses.sort((a, b) => {
+    if (a.sort_order == null && b.sort_order == null) return 0;
+    if (a.sort_order == null) return 1;
+    if (b.sort_order == null) return -1;
+    return a.sort_order - b.sort_order;
+  });
+  return courses;
 }
---
+++ public/js/index.js
@@ -0,0 +1,92 @@
+const ManageCourses = {
+  isDragging: false,
+
+  getTableBody: function () {
+    const table = document.getElementsByClassName("manage-courses-table")[0];
+    const tableBody = table && table.getElementsByTagName("tbody")[0];
+    if (!tableBody) {
+      console.error("tbody of 'Manage Courses' table not found.");
+      alert("ERROR: tbody of 'Manage Courses' table not found.");
+    }
+    return tableBody;
+  },
+
+  enableReorder: function (event) {
+    const tableBody = ManageCourses.getTableBody();
+    if (!tableBody) return false;
+
+    const defaultButtons = document.getElementsByClassName("manage-courses-default-buttons")[0];
+    defaultButtons.style.display = "none";
+
+    const orderingButtons = document.getElementsByClassName("manage-courses-ordering-buttons")[0];
+    orderingButtons.style.display = "flex";
+
+    const tableRows = Array.from(tableBody.getElementsByTagName("tr"));
+    tableRows.forEach(ManageCourses.makeDraggable);
+
+    const hiddenInput = document.getElementsByName("sorted_course_ids")[0];
+    hiddenInput.value = tableRows
+      .map(function (row) {
+        return row.dataset.courseId;
+      })
+      .join(",");
+  },
+
+  makeDraggable: function (rowElement) {
+    rowElement.draggable = true;
+    rowElement.ondragstart = ManageCourses.handleDragStart;
+    rowElement.ondragover = ManageCourses.handleDragOver;
+    rowElement.ondrop = ManageCourses.handleDrop;
+    rowElement.ondragend = ManageCourses.handleDragEnd;
+  },
+
+  makeNotDraggable: function (rowElement) {
+    rowElement.draggable = false;
+    rowElement.ondragstart = null;
+    rowElement.ondragover = null;
+    rowElement.ondrop = null;
+    rowElement.ondragend = null;
+  },
+
+  handleDragStart: function (event) {
+    ManageCourses.isDragging = true;
+    event.target.classList.add("manage-courses-dragging-row");
+  },
+
+  handleDragOver: function (event) {
+    event.preventDefault();
+    const draggedOverRow = event.target.closest("tr");
+    const tbody = draggedOverRow.parentNode;
+    const draggedRow = tbody.querySelector(".manage-courses-dragging-row");
+    if (!draggedRow) {
+      console.error("Unable to locate dragged row");
+      return;
+    }
+
+    if (ManageCourses.isDragging && draggedOverRow !== draggedRow) {
+      const positionDifference = draggedRow.offsetTop - draggedOverRow.offsetTop;
+      if (positionDifference < 0) {
+        tbody.insertBefore(draggedRow, draggedOverRow.nextSibling);
+      } else {
+        tbody.insertBefore(draggedRow, draggedOverRow);
+      }
+
+      const tableRows = Array.from(tbody.getElementsByTagName("tr"));
+      const hiddenInput = document.getElementsByName("sorted_course_ids")[0];
+      hiddenInput.value = tableRows
+        .map(function (row) {
+          return row.dataset.courseId;
+        })
+        .join(",");
+    }
+  },
+
+  handleDrop: function (event) {
+    event.preventDefault();
+  },
+
+  handleDragEnd: function (event) {
+    ManageCourses.isDragging = false;
+    event.target.classList.remove("manage-courses-dragging-row");
+  },
+};
