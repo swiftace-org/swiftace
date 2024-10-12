e73bc00823c1e64671f4f750ea89d997baed94ad
Aakash N S <1560745+aakashns@users.noreply.github.com>
2024-10-12 10:04:48 +0530

    Blank initial setup for Deno

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import { onGetHome } from "./pages/index.jsx";
-
-async function onRequest(request) {
-  return await onGetHome(request);
-}
-
-export default { fetch: onRequest };
--- src/lib/response.js
+++
@@ -1,6 +0,0 @@
-export function makeHtmlResponse(element, options) {
-  return new Response(element, {
-    headers: { "content-type": "text/html;charset=UTF-8", ...options?.headers },
-    ...options,
-  });
-}
